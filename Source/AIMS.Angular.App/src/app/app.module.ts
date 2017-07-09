ff8b32963acc757ede2df09064858698ac1ff033
jitendraprajapati <jitendra.prajapati@synapticdigital.com>
2017-07-09 14:17:39 +0530

    Added categories stub

--- Source/AIMS.Angular.App/src/app/app.module.ts
+++ Source/AIMS.Angular.App/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './navmenu/navmenu.component';
 import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './category/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
         { path: '', redirectTo: 'home', pathMatch: 'full' },
         { path: 'home', component: HomeComponent },
+        { path: 'categories', component: CategoriesComponent },
         { path: '**', redirectTo: 'home' }
     ])
   ],
---
+++ Source/AIMS.Angular.App/src/app/category/categories.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector :'categories',
+  templateUrl: './categories.component.html'  
+})
+
+export class CategoriesComponent {
+    
+}
