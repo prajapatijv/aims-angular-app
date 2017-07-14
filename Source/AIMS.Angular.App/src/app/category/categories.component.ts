c7f50c1203b8e42b1f9c35f4948d8a83d9355399
jitendraprajapati <jitendra.prajapati@synapticdigital.com>
2017-07-14 18:38:24 +0530

    Update component

--- Source/AIMS.Angular.App/src/app/category/categories.component.ts
+++ Source/AIMS.Angular.App/src/app/category/categories.component.ts
@@ -1,11 +1,12 @@
+//import {Component, ElementRef, ViewChild} from '@angular/core';
 import {Component} from '@angular/core';
 //import { MdTableModule } from '@angular/material';
 import {DataSource} from '@angular/cdk';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
-//import 'rxjs/add/operator/startWith';
-//import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -14,12 +15,14 @@ import 'rxjs/add/operator/map';
   templateUrl: './categories.component.html',
 })
 export class CategoriesComponent {
-  dataSource: Array<CategoryModel>;
+  dataSource: CategoryDataSource | null;
 
   constructor(private http:Http) {
   }
 
   ngOnInit() {
+    this.dataSource = new CategoryDataSource();
+    
       this.http
         .get('http://localhost:8085/api/categories')
         .map(response => response.json())
@@ -30,9 +33,12 @@ export class CategoriesComponent {
     }
 }
 
-
 export interface CategoryModel {
   code: number;
   name: string;
 }
 
+export class CategoryDataSource {
+   dataChange: BehaviorSubject<CategoryModel[]> = new BehaviorSubject<CategoryModel[]>([]);
+   get data() : CategoryModel[] {return this.dataChange.value; }
+}
